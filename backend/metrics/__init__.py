8fa3c1a5970481d47c2785a011b7858b02611c39
kynan <kynangallen14@gmail.com>
2026-09-08 15:29:19 +0800

    Implemented scaffold of TUM as per the docs.

--- backend/metrics/__init__.py
+++ backend/metrics/__init__.py
@@ -1,3 +1,7 @@
 """
-Time usage model
+Time usage model.
 """
+
+from backend.metrics.tum import Settings, Span, State, Utilisation, summarise
+
+__all__ = ["Settings", "Span", "State", "Utilisation", "summarise"]
